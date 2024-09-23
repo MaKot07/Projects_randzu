c003b7ee1a44ebadb5c093b26172d3b60872a4fa
MaKot07 <leha.semenov.07@yandex.ru>
2024-09-23 19:36:35 +0300

    Upgrade generation positions and start gen labels

--- PROJECT/neural_network-main.py
+++ PROJECT/neural_network-main.py
@@ -4,6 +4,7 @@ from Graphics.Graphics_main import *
 import sys
 from Brain_main import *
 import pandas as pd
+import pickle
 
 def generation_position_and_save(n, number_move):
 
@@ -11,14 +12,14 @@ def generation_position_and_save(n, number_move):
     white = 1
     cell_qty = 14
     color_move = black
-    percent_random = 0.6
-    percent_depth1 = 0.9
+    percent_random = 0.1
+    percent_depth1 = 0.4
 
-
-    for sample in range(n):
+    ni = 0
+    while ni < n:
         sgen_board = Board(black)
 
-        coord_move = (np.random.randint(5, 8), np.random.randint(5, 8))
+        coord_move = (np.random.randint(5, 7), np.random.randint(5, 7))
         sgen_board.set_coord(coord_move[0], coord_move[1], black)
         sgen_board.adding_lines(coord_move[0], coord_move[1], black)
 
@@ -37,28 +38,35 @@ def generation_position_and_save(n, number_move):
 
                 sgen_board.set_coord(coord_move[0], coord_move[1], color_move)
                 sgen_board.adding_lines(coord_move[0], coord_move[1], color_move)
+                make_position(sgen_board.give_chips(), [sgen_board.give_all_line_blackplayer(), sgen_board.give_all_line_whiteplayer()])
 
-                make_position(sgen_board.give_chips())
-
-            elif level_choice <= percent_depth1 and True:
+            elif level_choice <= percent_depth1:
                 maximizing = True if color_move == white else False
                 variants_move = P_generator_motion(coord_move, sgen_board.give_chips(), copy.copy(variants_move))
-                _, coord_move, c = sily_minimax(sgen_board, 1, variants_move, maximizing)
+
+                Board_SillyMinMax = Board(color_move, sgen_board.give_all_line_blackplayer(),
+                                     sgen_board.give_all_line_whiteplayer(), sgen_board.give_chips())
+
+                _, coord_move, c = sily_minimax(Board_SillyMinMax, 1, variants_move, maximizing)
 
                 sgen_board.set_coord(coord_move[0], coord_move[1], color_move)
                 sgen_board.adding_lines(coord_move[0], coord_move[1], color_move)
 
-                make_position(sgen_board.give_chips())
+                make_position(sgen_board.give_chips(), [sgen_board.give_all_line_blackplayer(), sgen_board.give_all_line_whiteplayer()])
 
             else:
                 maximizing = True if color_move == white else False
                 variants_move = P_generator_motion(coord_move, sgen_board.give_chips(), copy.copy(variants_move))
-                _, coord_move, _ = sily_minimax(sgen_board, 2, variants_move, maximizing)
+
+                Board_SillyMinMax = Board(color_move, sgen_board.give_all_line_blackplayer(),
+                                          sgen_board.give_all_line_whiteplayer(), sgen_board.give_chips())
+
+                _, coord_move, _ = sily_minimax(Board_SillyMinMax, 2, variants_move, maximizing)
 
                 sgen_board.set_coord(coord_move[0], coord_move[1], color_move)
                 sgen_board.adding_lines(coord_move[0], coord_move[1], color_move)
 
-                make_position(sgen_board.give_chips())
+                make_position(sgen_board.give_chips(), [sgen_board.give_all_line_blackplayer(), sgen_board.give_all_line_whiteplayer()])
 
             win_color = sgen_board.check_colors_win()
             if win_color != 0:
@@ -69,8 +77,11 @@ def generation_position_and_save(n, number_move):
             else:
                 color_move = black
 
+            ni += 1
 
-def make_position(all_coords):
+
+
+def make_position(all_coords, all_lines):
     len_board = 15
     position = np.zeros((225), dtype=np.int8)
 
@@ -81,6 +92,16 @@ def make_position(all_coords):
     with open(r'C:\Users\lehas\GitHub\Projects_randzu\PROJECT\neural_network\train_data.csv', 'a', newline='') as file:
         np.savetxt(file, [position], delimiter=',', newline='\n', fmt='%d')
 
+    # with open(r'C:\Users\lehas\GitHub\Projects_randzu\PROJECT\neural_network\train_data_for_labels(black).csv', 'a', newline='') as file:
+    #     np.savetxt(file, np.vstack(all_lines[0]), delimiter=',', newline='\n', fmt='%d')
+    with open(r'C:\Users\lehas\GitHub\Projects_randzu\PROJECT\neural_network\train_data_for_labels(black).csv', 'ab') as file:
+        pickle.dump(all_lines[0], file)
+
+
+    # with open(r'C:\Users\lehas\GitHub\Projects_randzu\PROJECT\neural_network\train_data_for_labels(white).csv', 'a', newline='') as file:
+    #     np.savetxt(file, np.vstack(all_lines[1]), delimiter=',', newline='\n', fmt='%d')
+    with open(r'C:\Users\lehas\GitHub\Projects_randzu\PROJECT\neural_network\train_data_for_labels(white).csv', 'ab') as file:
+        pickle.dump(all_lines[1], file)
 
 def sily_minimax(board_condition, depth, last_variants, maximizingPlayer, alpha=float('-inf'), beta=float('inf'), count_variants=0):
 
@@ -137,6 +158,29 @@ def sily_minimax(board_condition, depth, last_variants, maximizingPlayer, alpha=
 def show_positions():
     positions = np.loadtxt(r'C:\Users\lehas\GitHub\Projects_randzu\PROJECT\neural_network\train_data.csv', delimiter=',',
                      dtype=int)
+
+    # all_black_lines = np.loadtxt(r'C:\Users\lehas\GitHub\Projects_randzu\PROJECT\neural_network\train_data_for_labels(black).csv', delimiter=',',
+    #                  dtype=int)
+    with open(r'C:\Users\lehas\GitHub\Projects_randzu\PROJECT\neural_network\train_data_for_labels(black).csv', 'rb') as file:
+        all_black_lines = []
+        while True:
+            try:
+                all_black_lines.append(pickle.load(file))
+            except EOFError:
+                break
+
+
+    # all_white_lines = np.loadtxt(
+    #     r'C:\Users\lehas\GitHub\Projects_randzu\PROJECT\neural_network\train_data_for_labels(white).csv', delimiter=',',
+    #     dtype=int)
+    with open(r'C:\Users\lehas\GitHub\Projects_randzu\PROJECT\neural_network\train_data_for_labels(white).csv', 'rb') as file:
+        all_white_lines = []
+        while True:
+            try:
+                all_white_lines.append(pickle.load(file))
+            except EOFError:
+                break
+
     i = 0
 
     Visual_board = Game_Graphics(0)
@@ -153,6 +197,8 @@ def show_positions():
             if event.button == 1:
                 Visual_board = Game_Graphics(0, convert_positions(positions[i]))
                 Visual_board.draw_all_game(0)
+                print("BLACK", all_black_lines[i])
+                print("WHITE", all_white_lines[i])
 
                 i +=1
                 if i == len(positions):
@@ -179,8 +225,55 @@ def convert_positions(position):
     return conv_pos
 
 
+# def generation_labels():
+#     positions = np.loadtxt(r'C:\Users\lehas\GitHub\Projects_randzu\PROJECT\neural_network\train_data.csv',
+#                            delimiter=',',
+#                            dtype=int)
+#
+#     with open(r'C:\Users\lehas\GitHub\Projects_randzu\PROJECT\neural_network\train_data_for_labels(black).csv', 'rb') as file:
+#         all_black_lines = []
+#         while True:
+#             try:
+#                 all_black_lines.append(pickle.load(file))
+#             except EOFError:
+#                 break
+#
+#     with open(r'C:\Users\lehas\GitHub\Projects_randzu\PROJECT\neural_network\train_data_for_labels(white).csv', 'rb') as file:
+#         all_white_lines = []
+#         while True:
+#             try:
+#                 all_white_lines.append(pickle.load(file))
+#             except EOFError:
+#                 break
+#
+#     possible_moves_white_pl = typed.Dict.empty(
+#         key_type=types.UniTuple(types.int64, 2),
+#         value_type=numba.types.int64
+#     )
+#     possible_moves_black_pl = typed.Dict.empty(
+#         key_type=types.UniTuple(types.int64, 2),
+#         value_type=numba.types.int64
+#     )
+#
+#     for sample in len(all_black_lines):
+#         Board_MinMax = Board(positions[sample][-1],all_black_lines[sample],
+#                              all_white_lines[sample], convert_positions(positions[sample]))
+#
+#
+#
+#         next_variants_move_and_motion = ((index_x_rect, index_y_rect), create_independent_dict(possible_moves_black_pl),
+#                                          create_independent_dict(possible_moves_white_pl))
+#
+#         maxim = True if color_computer == white else False
+#         best_value, coord_best_move, count_all_variants = minimax(Board_MinMax, 40, next_variants_move_and_motion,
+#                                                                   maxim, float('-inf'), float('inf'), 0)
 
-generation_position_and_save(2, 50)
+
+
+
+
+
+#generation_position_and_save(100, 50)
 
 
 
